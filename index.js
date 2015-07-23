182466974428b8581953f97f8d626a5c1d1771c3
Frank Carver <frank@efsol.com>
2015-07-23 16:38:03 +0100

    got tougher on level sort order
    make sure it gets them in the right order
    even if the underlying file system doesn't.

--- index.js
+++ index.js
@@ -6,42 +6,44 @@ var async = require('async');
 var fs = require("fs");
 var path = require('path');
 
+function compare_levels(a, b) {
+  return Number(a) - Number(b);
+}
+
 exports.load = function load(root_path, next) {
   var ret = [];
   fs.readdir(root_path, function(err, levels) {
     if (err) return next(err);
 
-    async.forEach(levels, function(level_string, done) {
+    async.forEach(levels.sort(compare_levels), function(level_string, done) {
       var level_path = path.join(root_path, level_string);
-      fs.lstat(level_path, function(err, stats) {
-        if (err) return done(err);
-        if (!stats.isDirectory()) return done();
-        if (isNaN(level_string)) return done(new Error('Migrate level ' + level_string + ' is not a number. Migration aborted'));
+      var stats = fs.lstatSync(level_path);
+      if (err) return done(err);
+      if (!stats.isDirectory()) return done();
+      if (isNaN(level_string)) return done(new Error('Migrate level ' + level_string + ' is not a number. Migration aborted'));
 
-        var level_number = parseInt(level_string, 10);
-        var level = {
-          level: level_number,
-          up: []
-        };
-        fs.readdir(level_path, function(err, steps) {
+      var level_number = parseInt(level_string, 10);
+      var level = {
+        level: level_number,
+        up: []
+      };
+
+      var steps = fs.readdirSync(level_path);
+
+      async.forEach(steps.sort(), function(step, done) {
+        var step_file = path.join(level_path, step);
+        fs.readFile(step_file, "utf8", function (err,data) {
           if (err) return done(err);
 
-          async.forEach(steps.sort(), function(step, done) {
-            var step_file = path.join(level_path, step);
-            fs.readFile(step_file, "utf8", function (err,data) {
-              if (err) return done(err);
-
-              level.up.push(data);
-              done();
-            });
-          }, function(err) {
-            if (err) return done(err);
-
-            ret.push(level);
-            done();
-          });
+          level.up.push(data);
+          done();
         });
-      })
+      }, function(err) {
+        if (err) return done(err);
+
+        ret.push(level);
+        done();
+      });
     }, function(err) {
       next(err, err ? null : ret);
     });
--- test/test_all.js
+++ test/test_all.js
@@ -4,6 +4,9 @@
 var test = require('tape');
 var path = require('path');
 var util = require('util');
+var fs = require('fs');
+
+var rmdir = require('rimraf');
 
 var loader = require('../index');
 
@@ -38,7 +41,6 @@ test('valid with 1 level 1 step', function(t) {
   });
 });
 
-
 test('valid with 2 level 2 step', function(t) {
   t.plan(4);
   loader.load(path.join(__dirname, '../input/test2'), function(err, scripts) {
@@ -46,5 +48,35 @@ test('valid with 2 level 2 step', function(t) {
     t.equal(scripts.length, 2, 'test 1 should give 2 levels');
     t.equal(scripts[0].up.length, 1, 'test 1 should give 1 script in level 1');
     t.equal(scripts[1].up.length, 2, 'test 1 should give 2 scripts in level 2');
+  });
+});
+
+test('strange file sort order', function(t) {
+  t.plan(10);
+  var tmp = path.join(__dirname, '../tmp/A');
+  rmdir.sync(tmp);
+
+  fs.mkdirSync(tmp);
+
+  fs.mkdirSync(path.join(tmp, '2'));
+  fs.writeFileSync(path.join(tmp, '2/z'), 'po');
+  fs.writeFileSync(path.join(tmp, '2/a'), 'lala');
+
+  fs.mkdirSync(path.join(tmp, '1'));
+  fs.writeFileSync(path.join(tmp, '1/23'), "dipsy");
+  fs.writeFileSync(path.join(tmp, '1/12'), "tinky-winky");
+
+  loader.load(tmp, function(err, scripts) {
+    t.error(err, 'load of out-of-order levels should not error');
+    t.equal(scripts.length, 2, 'should give 2 levels');
+    t.equal(scripts[0].level, 1, 'should read level 1 first');
+    t.equal(scripts[0].up.length, 2, 'should give 2 scripts in level 1');
+    t.equal(scripts[1].level, 2, 'should read level 2 next');
+    t.equal(scripts[1].up.length, 2, 'should give 2 scripts in level 2');
+
+    t.equal(scripts[0].up[0], 'tinky-winky', 'should give scripts in correct order 1');
+    t.equal(scripts[0].up[1], 'dipsy', 'should give scripts in correct order 2');
+    t.equal(scripts[1].up[0], 'lala', 'should give scripts in correct order 3');
+    t.equal(scripts[1].up[1], 'po', 'should give scripts in correct order 4');
   });
 });
